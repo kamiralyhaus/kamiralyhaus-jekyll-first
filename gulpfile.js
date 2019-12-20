fc5f4c9e4bfdef92da1f340c94ac1e03301e91b8
Ралица Камарашева <ralitsa.kamarasheva@gmail.com>
2019-12-20 16:53:23 +0200

    Adding Gulp automation

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+"use strict";
+
+let gulp = require("gulp"),
+    autoprefixer = require("gulp-autoprefixer"),
+    exec=require("gulp-exec"),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
+	cp = require("child_process");
+
+gulp.task("css", function() {
+	return gulp.src( '_assets/css/**/*.css' )
+		// .pipe( size() )
+		// .pipe( sass().on('error', sass.logError) )
+		.pipe( autoprefixer() )
+		// .pipe( size() )
+		// .pipe( csso() )
+		// .pipe( size() )
+		.pipe( gulp.dest( './docs/css/' ) )
+		.pipe( browserSync.stream({ match: '**/*.css' }) )
+	;
+});
+
+// Jekyll
+/* gulp.task("jekyll-dev", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl ''"], { stdio: "inherit", shell: true });
+}); */
+
+// Jekyll
+gulp.task("jekyll", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+});
+
+gulp.task("watch", function() {
+
+	browserSync.init({
+		server: {
+            baseDir: "./docs/"
+		}
+	});
+
+	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+
+	gulp.watch(
+		[
+			"./*.html",
+			"./*.yml",
+			"./_includes/*.html",
+			"./_layouts/*.html",
+			"./_posts/**/*.*"
+		]
+	).on('change', gulp.series('jekyll', 'css') );
+
+	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
+	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
+});
+
+
+/* gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
+	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
+})); */
+
+// gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
+gulp.task("default", gulp.series('jekyll', 'css', 'watch'));
